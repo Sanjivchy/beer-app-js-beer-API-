200c2d69eb62d9d6cf40044d190a6efdfcb47ba7
xoonziip <sanjivchaudhary416@gmail.com>
2021-03-15 12:33:34 +0545

    js/css modified

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// variables
+const urlBase = "https://api.punkapi.com/v2/beers";
+
+async function getBeers() {
+    // fetch
+    const beerPromise = await fetch(urlBase);
+    const beers = await beerPromise.json();
+    
+    // render data
+    const beersDiv = document.querySelector('.beers');
+    
+    let beerHtml = "";
+     
+    console.log(beers[0]);
+    // Add the rest of the beer data to the HTML template.
+    
+    beers.forEach(beer => {
+       beerHtml += `
+        <div class='beer-wrapper card'>
+            <div class='beer'>
+                <img class='beer__img' src="${beer.image_url}">
+                <h3>${beer.name}</h3>
+                <span class='beer__info'>
+                    <span>ABV: ${beer.abv}%</span>
+                    <span>IBU: ${beer.ibu}</span>
+                </span>
+            </div>
+            <div class='beer__content'>
+                <div class='beer__name'>${beer.name}</div>
+                <div class='beer__tagline'>${beer.tagline}</div>
+                <div class='beer__description'>${beer.description}</div>
+                <div class='beer__food-pairing'>
+                    pair with:${beer.food_pairing.join(', ')}             
+                </div>
+            </div>
+        </div>
+       `; 
+    });
+    
+    beersDiv.innerHTML = beerHtml;
+}
+
+// initial get
+getBeers();
